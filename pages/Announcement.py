d0340d8422f402a98bd7fe9efa7ae3d7d2f443ea
DasFranki <47039743+DasFranki@users.noreply.github.com>
2024-03-01 19:40:30 +0200

    announcements

---
+++ pages/Announcement.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Announcements")
+
+st.info("News")
+
+'''
+Welcome to the F1 - 10th Place Cup, where the thrill of Formula 1 racing meets the excitement of prediction!
+
+Our app brings you into the heart of the F1 action, allowing you to predict the 10th place driver in each race and earn points based on your predictions. It's not just about cheering for your favorite drivers; it's about testing your knowledge of the sport and seeing how your predictions stack up against others.
+
+Whether you're a seasoned F1 fan or new to the world of motorsport, the F1 - 10th Place Cup offers you a chance to engage with the races in a whole new way. With every race, you have the opportunity to make your mark and climb the leaderboard.
+
+To get started, simply register for an account or log in if you're already a member. Once logged in, you'll have access to the upcoming race schedule, where you can make your predictions for the 10th place driver. Will you go with a seasoned veteran or take a chance on an up-and-coming talent?
+
+Don't worry if you're new to prediction games; our app makes it easy and fun to participate. And remember, it's not just about winning—it's about joining a community of passionate F1 fans who share your love for the sport.
+
+So buckle up, get ready to experience the adrenaline of F1 racing like never before, and may the best predictor win!
+'''
