1d23ba09623fdd1866557d95f38d7a8452c58fea
DasFranki <47039743+DasFranki@users.noreply.github.com>
2024-03-03 18:55:09 +0200

    new feature

--- pages/Info.py
+++ pages/Info.py
@@ -9,7 +9,8 @@ if st.button("Don't touch"):
     st.error("I can't believe you've done this!")
 
 st.markdown('#### :green[Features]')
-st.text('2024-03-02 : Races Page added.')   
+st.text('2024-03-03 : Points column added to view how many points your previous picks scored.')   
+st.text('2024-03-02 : Races Page added.')
 
         
 
